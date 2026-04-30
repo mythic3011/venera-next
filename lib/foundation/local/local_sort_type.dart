8154e9e991639fb14b41d2ee7e8bc07b8a677001
Mythic3011 <42365807+mythic3011@users.noreply.github.com>
2026-04-30 20:55:12 +0800

    refactor(local): split local comic and sort types

---
+++ lib/foundation/local/local_sort_type.dart
@@ -0,0 +1,20 @@
+part of '../local.dart';
+
+enum LocalSortType {
+  name("name"),
+  timeAsc("time_asc"),
+  timeDesc("time_desc");
+
+  final String value;
+
+  const LocalSortType(this.value);
+
+  static LocalSortType fromString(String value) {
+    for (var type in values) {
+      if (type.value == value) {
+        return type;
+      }
+    }
+    return name;
+  }
+}
